39c4161e624391f78b14f4c3c52550a44da3bef1
Niño Moi <benalcazarsebas8173@gmail.com>
2025-07-01 19:14:54 -0500

    ..

--- computer-sales-api/index.js
+++ computer-sales-api/index.js
@@ -13,8 +13,13 @@ db.once("open", () => console.log("System connected to MongoDB Database"));
 
 app.use(express.json());
 
-const computerRouter = require("./routes/computersRoutes");
+// Ruta raíz para que no dé el error "Cannot GET /"
+app.get("/", (req, res) => {
+  res.send("¡Bienvenido a la tienda de computadoras!");
+});
 
+// Ruta para el endpoint /computerstore
+const computerRouter = require("./routes/computersRoutes");
 app.use("/computerstore", computerRouter);
 
 app.listen(port, () => console.log(`MY Computer Store Server is running on port --> ${port}`));
